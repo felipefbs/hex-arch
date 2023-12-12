723d0b0808d80e3d2ef391a74aa24ffdfad8f473
Felipe Ferreira <felipe.ferreira@theia.com.br>
2023-12-12 16:44:09 -0300

    working mock test

--- application/service_test.go
+++ application/service_test.go
@@ -3,8 +3,11 @@ package application_test
 import (
 	"testing"
 
+	"github.com/felipefbs/hex-arch/application"
 	mock_application "github.com/felipefbs/hex-arch/application/mocks"
 	"github.com/golang/mock/gomock"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestProductService(t *testing.T) {
@@ -15,4 +18,12 @@ func TestProductService(t *testing.T) {
 	persistence := mock_application.NewMockIProductPersistence(ctrl)
 
 	persistence.EXPECT().Get(gomock.Any()).Return(product, nil).AnyTimes()
+
+	svc := application.ProductService{
+		Persistence: persistence,
+	}
+
+	productFound, err := svc.Get(uuid.NewString())
+	assert.Nil(t, err)
+	assert.NotEmpty(t, productFound)
 }
